70b8a05af8387882582d2898d5f92b9c0c3f5120
Andrew Shieh <shieh.andrew@gmail.com>
2020-07-28 04:18:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Word Hunt Solver
+
+A simple, lightweight, no-frills solver for Word Hunt. Word Hunt is an iMessage game where two players compete to form 
+as many words as possible from a 4x4 letter grid in 80s by connecting adjacent letters together.
+
+# Installation
+
+- Compile `solver.cpp`
+- Run `./solver <board>`. `<board>` is the gameboard when read left to right
+```
+Board          Input
+
+a b c d        abcdefghijklmnop
+e f g h
+i j k l
+m n o p
+```
---
+++ solver.cpp
@@ -0,0 +1,105 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+const int N = 4;
+const vector<pair<int, int>> DIRS = {
+    make_pair(-1,-1), make_pair(-1, 0), make_pair(-1, 1),
+    make_pair( 0,-1), make_pair( 0, 0), make_pair( 0, 1),
+    make_pair( 1,-1), make_pair( 1, 0), make_pair( 1, 1)
+};
+
+struct TrieNode {
+    bool is_word = false;
+    struct TrieNode *children[26] = {};
+};
+
+TrieNode *root = new TrieNode;
+vector<string> answer;
+
+void insert(string word) {
+    TrieNode *curr = root;
+    for (int i = 0; i < word.length(); i++) {
+        int j = word[i] - 'A';
+        if (!curr->children[j]) {
+            curr->children[j] = new TrieNode;
+        }
+        curr = curr->children[j];
+    }
+    curr->is_word = true;
+}
+
+bool prefix(string word) {
+    TrieNode *curr = root;
+    for (int i = 0; i < word.length(); i++) {
+        int j = word[i] - 'A';
+        if (!curr->children[j]) {
+            return false;
+        }
+        curr = curr->children[j];
+    }
+    return true;
+}
+
+bool check(string word) {
+    TrieNode *curr = root;
+    for (int i = 0; i < word.length(); i++) {
+        int j = word[i] - 'A';
+        if (!curr->children[j]) {
+            return false;
+        }
+        curr = curr->children[j];
+    }
+    return (curr && curr->is_word);
+}
+
+void dfs(string board, string word, int i, int j) {
+    if (i < 0 || j < 0 || i >= N || j >= N || board[i*N+j] == '*' || !prefix(word)) {
+        return;
+    }
+    if (check(word)) {
+        answer.push_back(word);
+    }
+    board[i*N+j] = '*';
+    for (pair<int, int> p: DIRS) {
+        int x = p.first;
+        int y = p.second;
+        dfs(board, word+board[(i+x)*N+j+y], i+x, j+y);
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cout << "Error: expected 1 argument, got " << argc-1 << "\n";
+        return 1;
+    }
+    string board = argv[1];
+    for (char &c: board) c = toupper(c);
+
+    ifstream dict("dictionary.txt");
+    string word;
+    while (dict >> word) {
+        insert(word);
+    }
+    dict.close();
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            dfs(board, string(1,board[i*N+j]), i, j);
+        }
+    }
+
+    sort(answer.begin(), answer.end(),
+     [](string &one, string &two) {
+            return one.length() == two.length() ? one < two : one.length() > two.length();
+         });
+    for (string s: answer) {
+        if (s.length() >= 3) {
+            cout << s << "\n";
+        }
+    }
+    return 0;
+}
